c2ba03e49ef9cb2ae5e46bd7ecb96da920320642
anastasia-levina <redheadgirl.lv@gmail.com>
2021-04-09 21:03:30 +0300

    0.2. FileInputStream[4898#338552]

---
+++ chapter_002/src/main/java/io/EvenNumberFile.java
@@ -0,0 +1,23 @@
+package io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                String rsl = Integer.parseInt(line) % 2 == 0 ? " even" : " odd";
+                System.out.println(line + rsl);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

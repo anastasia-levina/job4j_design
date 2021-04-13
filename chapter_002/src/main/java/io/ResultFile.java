73868d2a526a71159e7cf6514b7df2261c772c78
anastasia-levina <redheadgirl.lv@gmail.com>
2021-04-14 00:34:16 +0300

    0.1. Рефакторинг - FileOutputStream.[252488#338542]

--- chapter_002/src/main/java/io/ResultFile.java
+++ chapter_002/src/main/java/io/ResultFile.java
@@ -5,11 +5,11 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write(("1  2  3  4  5\n" +
-                    "2  4  6  8  10\n" +
-                    "3  6  9  12 15\n" +
-                    "4  8  12 16 20\n" +
-                    "5  10 15 20 25\n").getBytes());
+            out.write(("1  2  3  4  5" + System.lineSeparator() +
+                    "2  4  6  8  10" + System.lineSeparator() +
+                    "3  6  9  12 15" + System.lineSeparator() +
+                    "4  8  12 16 20" + System.lineSeparator() +
+                    "5  10 15 20 25" + System.lineSeparator()).getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }

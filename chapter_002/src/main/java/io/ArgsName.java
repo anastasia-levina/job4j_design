7f66d112b05c7af1d78fce582852e09168861529
anastasia-levina <redheadgirl.lv@gmail.com>
2021-05-12 17:19:23 +0300

    5.1. Именованные аргументы[295518#338546]

---
+++ chapter_002/src/main/java/io/ArgsName.java
@@ -0,0 +1,40 @@
+package io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        for (String a : args) {
+            if (a.indexOf("=") == a.length() - 1) {
+                throw new IllegalArgumentException();
+            }
+            String[] split = a.replaceFirst("-", "").split("=");
+            values.put(split[0], split[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
